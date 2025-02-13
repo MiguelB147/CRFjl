668c5d9586f16ecc01c47a641918afa23e2f6c17
Miguel Beynaerts <miguel.beynaerts@gmail.com>
2025-02-13 20:58:19 +0100

    Implementation of full likelihood

--- CRF.jl
+++ CRF.jl
@@ -77,14 +77,13 @@ Riskset = function (x::Array, y::Array)
     return riskset
 end
 
-SimData = function(;K::Int, df::Int, degree::Int, ub = 0)
+SimData = function(;K::Int, df::Int, degree::Int, ub = 0; alpha = 0.0023)
 
     Random.seed!(123)
 
     u1 = rand(Uniform(0,1), K)
     u2 = rand(Uniform(0,1), K)
     
-    alpha = 0.0023
     a = alpha.^u1 + (alpha .- alpha.^u1).*u2
     
     # Fan 2000
@@ -134,21 +133,30 @@ SimData = function(;K::Int, df::Int, degree::Int, ub = 0)
 return (X = X, knots = hcat(knots1, knots2), delta = deltaprod, riskset = N, I1 = I1, I2 = I2, I5 = I5, I6 = I6)
 end
 
-LogLik = function (;riskset::Matrix, logtheta::Matrix, delta::Matrix, I1::Matrix, I2::Matrix, I3::Matrix)
+LogLik = function (; riskset::Matrix, logtheta::Matrix, delta::Matrix, I1::Matrix, I2::Matrix, I5::Matrix, I6::Matrix)
 
     n = size(riskset, 1)
-    sum::Float64 = 0
+    sum1::Float64 = 0
+    sum2::Float64 = 0
 
     for i in 1:n
         for j in 1:n
-            if riskset[i,j] > 0
-                sum += delta[i,j]*I1[i,j]*(logtheta[i,j]*I3[i,j] - log(riskset[i,j] + I2[i,j]*(exp(logtheta[i,j]) - 1)))
+            if riskset[j,i] > 0
+                sum1 += delta[j,i]*I1[i,j]*(logtheta[j,i]*I5[i,j] - log(riskset[j,i] + I2[i,j]*(exp(logtheta[j,i]) - 1)))
             else
-                 sum += 0
+                 sum1 += 0
             end
+
+            if riskset[i,j] > 0
+                sum2 += delta[i,j]*I2[j,i]*(logtheta[i,j]*I6[i,j] - log(riskset[i,j] + I1[j,i]*(exp(logtheta[i,j]) - 1)))
+            else
+                sum2 += 0
+            end
+
+
         end
     end
-    return -sum;
+    return -sum1-sum2;
 end
 
 spline = function(t1::Vector, t2::Vector, coef::Vector; degree::Int, knots::Matrix)
@@ -162,6 +170,13 @@ spline = function(t1::Vector, t2::Vector, coef::Vector; degree::Int, knots::Matr
     tensor = B1 * coefmat * B2'
 
     return tensor
+end
+
+polynomial = function(t1::Vector, t2::Vector, coef::Vector)
+
+    X = hcat(fill(1,length(t1)), t1, t2, (t1).^2, (t2).^2, t1 .* t2, ((t1).^2).* t2, ((t2).^2).* t1, t1 .^3, t2 .^3)
+    
+    return X * coef
 end
 
 Profile = function (datalist::NamedTuple, degree::Int; start::Vector, lower::Int, upper::Int, step::Float64, iter = 10)
@@ -183,7 +198,7 @@ Profile = function (datalist::NamedTuple, degree::Int; start::Vector, lower::Int
             for k in 1:gridsize
                 result[i,j] = grid[k]
                 logtheta = spline(datalist.X[:,1], datalist.X[:,2], result[i,:], degree = degree, knots = datalist.knots)
-                ll[k] = LogLik(riskset = datalist.riskset, logtheta = logtheta, delta = datalist.delta, I1 = datalist.I1, I2 = datalist.I2, I3 = datalist.I5)
+                ll[k] = LogLik(riskset = datalist.riskset, logtheta = logtheta, delta = datalist.delta, I1 = datalist.I1, I2 = datalist.I2, I5 = datalist.I5, I6 = datalist.I6)
             end
             result[i,j] = grid[argmin(ll)]
             println("Iteration ", i, ", Parameter ", j, ": ", result[i,j])
@@ -220,7 +235,7 @@ for i in 1:iter
             for k in 1:gridsize
                 result[i,j] = grid[k]
                 logtheta = spline(datalist.X[:,1], datalist.X[:,2], result[i,:], degree = degree, knots = datalist.knots)
-                ll[k] = LogLik(riskset = datalist.riskset, logtheta = logtheta, delta = datalist.delta, I1 = datalist.I1, I2 = datalist.I2, I3 = datalist.I5)
+                ll[k] = LogLik(riskset = datalist.riskset, logtheta = logtheta, delta = datalist.delta, I1 = datalist.I1, I2 = datalist.I2, I5 = datalist.I5, I6 = datalist.I6)
             end
 
             result[i,j] = grid[argmin(ll)]
--- test.jl
+++ test.jl
@@ -2,23 +2,25 @@
 include("CRF.jl")
 
 using .CRF
+using Plots
 
 testdata = SimData(K=1000, df = 5, degree = 2, ub = 0)
 
-logtheta = spline(testdata.X[:,1], testdata.X[:,2], fill(1,25), degree = 2, knots = testdata.knots)
-LogLik(riskset = testdata.riskset, logtheta = logtheta, delta = testdata.delta, I1 = testdata.I1, I2 = testdata.I2, I3 = testdata.I5)
+logtheta = spline(testdata.X[:,1],testdata.X[:,2], fill(1,25), degree = 2, knots = testdata.knots)
+LogLik(riskset = testdata.riskset, logtheta = logtheta, delta = testdata.delta, I1 = testdata.I1, I2 = testdata.I2, I5 = testdata.I5, I6 = testdata.I6)
 
-solution = Profile(testdata, 2, start = fill(1,25), lower = -20, upper = 20, step = 0.1, iter = 20)
+solution = Profile(testdata, 2, start = ones(25), lower = -20, upper = 20, step = 0.1, iter = 5)
 
-solution = AdaptiveProfile(testdata, 2, start = fill(1,25), step = 0.1, iter = 50)
+solution = AdaptiveProfile(testdata, 2, start = ones(25), step = 0.1, iter = 10)
 
 
-grid = collect(-20:0.1:20)
+grid = collect(15:0.1:50)
 gridsize = length(grid)
 ll = Array{Float64}(undef, gridsize)
-coef = fill(1.000000,25)
+# coef = ones(25)
+coef = solution[5,:]
 for i in 1:gridsize
-    coef[25] = grid[i]
+    coef[5] = grid[i]
     logtheta = spline(testdata.X[:,1], testdata.X[:,2], coef, degree = 2, knots = testdata.knots)
-    ll[i] = LogLik(riskset = testdata.riskset, logtheta = logtheta, delta = testdata.delta, I1 = testdata.I1, I2 = testdata.I2, I3 = testdata.I5)
+    ll[i] = LogLik(riskset = testdata.riskset, logtheta = logtheta, delta = testdata.delta, I1 = testdata.I1, I2 = testdata.I2, I5 = testdata.I5, I6 = testdata.I6)
 end
